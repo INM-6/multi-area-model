Add scripts for Figure 7, integrate into snakemake workflow Theory class: add method d_nu and fix computation of gain matrix, rename stability_matrix to gain_matrix

multiarea_model/theory.py
@@ -305,7 +305,36 @@ class Theory():
 
         return mu, sigma
 
-    def stability_matrix(self, rates, matrix_filter=None,
+    def d_nu(self, mu, sigma):
+        """
+        Compute the derivative of the Siegert function by mu and sigma
+        for given mu and sigma.
+
+        Parameters
+        ----------
+        mu : numpy.ndarray
+            Mean input to the populations
+        sigma : numpy.ndarray
+            Variance of input to the populations
+        """
+        d_nu_d_mu = np.array([d_nu_d_mu_fb_numeric(1.e-3*self.NP['tau_m'],
+                                                   1.e-3*self.NP['tau_syn'],
+                                                   1.e-3*self.NP['t_ref'],
+                                                   self.NP['theta'],
+                                                   self.NP['V_reset'],
+                                                   mu[ii], sigma[ii]) for ii in range(len(mu))])
+        # Unit: 1/(mV)**2
+        d_nu_d_sigma = np.array([d_nu_d_sigma_fb_numeric(1.e-3*self.NP['tau_m'],
+                                                         1.e-3*self.NP['tau_syn'],
+                                                         1.e-3*self.NP['t_ref'],
+                                                         self.NP['theta'],
+                                                         self.NP['V_reset'],
+                                                         mu[ii], sigma[ii])*1/(
+                                                             2. * sigma[ii])
+                                 for ii in range(len(mu))])
+        return d_nu_d_mu, d_nu_d_sigma
+
+    def gain_matrix(self, rates, matrix_filter=None,
                          vector_filter=None, full_output=False):
         """
         Computes stability matrix on the population level.
@@ -349,24 +378,10 @@ class Theory():
         if np.any(vector_filter is not None):
             mu = mu[vector_filter]
             sigma = sigma[vector_filter]
-        slope = np.array([d_nu_d_mu_fb_numeric(1.e-3*self.NP['tau_m'],
-                                               1.e-3*self.NP['tau_syn'],
-                                               1.e-3*self.NP['t_ref'],
-                                               self.NP['V_th'],
-                                               self.NP['V_reset'],
-                                               mu[ii], sigma[ii]) for ii in range(N.size)])
 
-        # Unit: 1/(mV)**2
-        slope_sigma = np.array([d_nu_d_sigma_fb_numeric(1.e-3*self.NP['tau_m'],
-                                                        1.e-3*self.NP['tau_syn'],
-                                                        1.e-3*self.NP['t_ref'],
-                                                        self.NP['V_th'],
-                                                        self.NP['V_reset'],
-                                                        mu[ii], sigma[ii])*1/(
-                                                            2. * sigma[ii])
-                                for ii in range(N.size)])
+        d_nu_d_mu, d_nu_d_sigma = self.d_nu(mu, sigma)
 
-        slope_matrix = np.zeros_like(J)
+        slope_mu_matrix = np.zeros_like(J)
         slope_sigma_matrix = np.zeros_like(J)
         for ii in range(N.size):
             slope_mu_matrix[:, ii] = d_nu_d_mu
@@ -398,18 +413,17 @@ class Theory():
             contributing. Defaults to False.
         """
         if full_output:
-            (M, slope, slope_sigma,
-             M, EV, C, V, G_N) = self.stability_matrix(rates,
+            (G, slope, slope_sigma) = self.gain_matrix(rates,
                                                        matrix_filter=matrix_filter,
                                                        vector_filter=vector_filter,
                                                        full_output=full_output)
         else:
-            M = self.stability_matrix(rates, matrix_filter=matrix_filter,
-                                      vector_filter=vector_filter,
-                                      full_output=full_output)
-        EV = np.linalg.eig(M)
+            G = self.gain_matrix(rates, matrix_filter=matrix_filter,
+                                 vector_filter=vector_filter,
+                                 full_output=full_output)
+        EV = np.linalg.eig(G)
         lambda_max = np.sqrt(np.max(np.real(EV[0])))
         if full_output:
-            return lambda_max, slope, slope_sigma, M, EV, C, V
+            return lambda_max, slope, slope_sigma, G, EV
         else:
             return lambda_max