565d4fa591f8b646f328ad8b3f08f8280346fffa
Maximilian Schmidt <max.schmidt@fz-juelich.de>
2018-05-25 18:44:38 +0900

    Set paths to default None

--- figures/Schmidt2018_dyn/helpers.py
+++ figures/Schmidt2018_dyn/helpers.py
@@ -2,10 +2,10 @@ import numpy as np
 
 # Define path to original data (see README file for more information
 # how to download the original data)
-original_data_path = '/media/schmidt/3f02772f-a786-428b-8a91-a22e0745e052/MAM_paper/datasets/Simulations/data_dynamics_manuscript/data_publish'
+original_data_path = None
 
 # Define path to the experimental spiking data of Chu et al. (2014) (see README)
-chu2014_path = '/home/schmidt/Documents/work/Juelich/manuscripts/multiarea_manuscript/data/Chu2014_data/crcns-pvc5/'
+chu2014_path = None
 
 population_labels = ['2/3E', '2/3I', '4E', '4I', '5E', '5I', '6E', '6I']
 
