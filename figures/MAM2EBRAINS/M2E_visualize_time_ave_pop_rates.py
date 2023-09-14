4f613e28e5a44d43f8026b3a3eeed3bc6d27839a
didihou <didi.hou@rwth-aachen.de>
2023-09-14 18:45:34 +0200

    /

--- figures/MAM2EBRAINS/M2E_visualize_time_ave_pop_rates.py
+++ figures/MAM2EBRAINS/M2E_visualize_time_ave_pop_rates.py
@@ -35,6 +35,7 @@ def plot_time_averaged_population_rates(M, area_list=None, **keywords):
     matrix = np.zeros((len(area_list), len(A.network.structure['V1'])))
 
     fig = plt.figure(figsize=(16, 4))
+    fig.suptitle('Time-averaged population rates encoded in colors', fontsize=15, y=1.05)
     ax = fig.add_subplot(111)
 
     for i, area in enumerate(area_list):
