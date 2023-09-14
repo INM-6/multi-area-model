8c81845f2dc1f9d577f94f9cb4cf4333982d7d6e
didihou <didi.hou@rwth-aachen.de>
2023-09-14 13:57:44 +0200

    /

--- figures/MAM2EBRAINS/M2E_visualize_interareal_connectivity.py
+++ figures/MAM2EBRAINS/M2E_visualize_interareal_connectivity.py
@@ -36,7 +36,7 @@ def visualize_interareal_connectivity(M):
     pl.rcParams['figure.figsize'] = (width, height)
 
     fig = pl.figure()
-    fig.suptitle('Interareal connectivity for full-scale (left) and down-scale (right) multi-area model', fontsize=14, y=1.05)
+    fig.suptitle('Interareal connectivity for full-scale (left) and down-scale (right) multi-area model', fontsize=14, y=1.1)
     axes = {}
 
     # gs1 = gridspec.GridSpec(2, 2)
@@ -122,11 +122,13 @@ def visualize_interareal_connectivity(M):
 
     ax.set_xticks([i + 0.5 for i in np.arange(0, len(area_list) + 1, 1)])
     ax.set_xticks([i + 0.5 for i in np.arange(0, len(area_list), 1)])
-    ax.set_xticklabels(area_list, rotation=90, size=6.)
+    # ax.set_xticklabels(area_list, rotation=90, size=6.)
+    ax.set_xticklabels(area_list, rotation=90, size=10.)
 
     # ax.set_yticks([i + 0.5 for i in np.arange(0, len(area_list) + 1, 1)])
     ax.set_yticks([i + 0.5 for i in np.arange(0, len(area_list), 1)])
-    ax.set_yticklabels(area_list[::-1], size=6.)
+    # ax.set_yticklabels(area_list[::-1], size=6.)
+    ax.set_yticklabels(area_list[::-1], size=10.)
 
     ax.set_ylabel('Target area')
     ax.set_xlabel('Source area')
@@ -195,11 +197,13 @@ def visualize_interareal_connectivity(M):
 
     # ax.set_xticks([i + 0.5 for i in np.arange(0, len(area_list) + 1, 1)])
     ax.set_xticks([i + 0.5 for i in np.arange(0, len(area_list), 1)])
-    ax.set_xticklabels(area_list, rotation=90, size=6.)
+    # ax.set_xticklabels(area_list, rotation=90, size=6.)
+    ax.set_xticklabels(area_list, rotation=90, size=10.)
 
     # ax.set_yticks([i + 0.5 for i in np.arange(0, len(area_list) + 1, 1)])
     ax.set_yticks([i + 0.5 for i in np.arange(0, len(area_list), 1)])
-    ax.set_yticklabels(area_list[::-1], size=6.)
+    # ax.set_yticklabels(area_list[::-1], size=6.)
+    ax.set_yticklabels(area_list[::-1], size=10.)
 
     ax.set_ylabel('Target area')
     ax.set_xlabel('Source area')
@@ -322,11 +326,13 @@ def visualize_interareal_connectivity(M):
 
     # ax.set_xticks([i + 0.5 for i in np.arange(0, len(area_list) + 1, 1)])
     ax.set_xticks([i + 0.5 for i in np.arange(0, len(area_list), 1)])
-    ax.set_xticklabels(area_list, rotation=90, size=6.)
+    # ax.set_xticklabels(area_list, rotation=90, size=6.)
+    ax.set_xticklabels(area_list, rotation=90, size=10.)
 
     # ax.set_yticks([i + 0.5 for i in np.arange(0, len(area_list) + 1, 1)])
     ax.set_yticks([i + 0.5 for i in np.arange(0, len(area_list), 1)])
-    ax.set_yticklabels(area_list[::-1], size=6.)
+    # ax.set_yticklabels(area_list[::-1], size=6.)
+    ax.set_yticklabels(area_list[::-1], size=10.)
 
     ax.set_ylabel('Target area')
     ax.set_xlabel('Source area')
