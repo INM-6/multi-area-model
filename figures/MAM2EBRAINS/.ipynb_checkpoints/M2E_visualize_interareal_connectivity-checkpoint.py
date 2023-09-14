12ff693395d8a97a1a80a4b75da26ff5382ba663
didihou <didi.hou@rwth-aachen.de>
2023-09-14 13:30:46 +0200

    /

--- figures/MAM2EBRAINS/.ipynb_checkpoints/M2E_visualize_interareal_connectivity-checkpoint.py
+++ figures/MAM2EBRAINS/.ipynb_checkpoints/M2E_visualize_interareal_connectivity-checkpoint.py
@@ -189,10 +189,12 @@ def visualize_interareal_connectivity(M):
     y = np.arange(0, len(area_list[::-1]) + 1)
     X, Y = np.meshgrid(x, y)
 
-    ax.set_xticks([i + 0.5 for i in np.arange(0, len(area_list) + 1, 1)])
+    # ax.set_xticks([i + 0.5 for i in np.arange(0, len(area_list) + 1, 1)])
+    ax.set_xticks([i + 0.5 for i in np.arange(0, len(area_list), 1)])
     # ax.set_xticklabels(area_list, rotation=90, size=6.)
 
-    ax.set_yticks([i + 0.5 for i in np.arange(0, len(area_list) + 1, 1)])
+    # ax.set_yticks([i + 0.5 for i in np.arange(0, len(area_list) + 1, 1)])
+    ax.set_yticks([i + 0.5 for i in np.arange(0, len(area_list), 1)])
     # ax.set_yticklabels(area_list[::-1], size=6.)
 
     ax.set_ylabel('Target area')
